cb82e7421c648124c4e6a60af1492af78d3a79e2
unknown <ashwanipandey1218@gmail.com>
2018-12-31 12:46:29 +0530

    First Commit

---
+++ src/main/java/com/ashwani/RecipeApplication/Controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.ashwani.RecipeApplication.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"","/","index"})
+	public String getIndexPage() {
+		return "index";
+	}
+}
